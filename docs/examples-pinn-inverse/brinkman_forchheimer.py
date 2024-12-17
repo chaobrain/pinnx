dbff31590e3ccd88e17e6861f2e32bba2d77dda1
Chaoming Wang <adaduo@outlook.com>
2024-12-17 17:36:31 +0800

    add dimensional analysis for Brinkman Forchheimer Problem

--- docs/examples-pinn-inverse/brinkman_forchheimer.py
+++ docs/examples-pinn-inverse/brinkman_forchheimer.py
@@ -2,33 +2,34 @@
 Implementation of Brinkman-Forchheimer equation example in paper https://arxiv.org/pdf/2111.02801.pdf.
 """
 
-import re
-
 import brainstate as bst
-import matplotlib.pyplot as plt
+import brainunit as u
 import numpy as np
 
 import pinnx
 
-g = 1
-v = 1e-3
+g = 1 * u.meter / u.second ** 2
+v = 1e-3 * u.meter ** 2 / u.second
 e = 0.4
-H = 1
+H = 1 * u.meter
 
-v_e = bst.ParamState(0.1)
-K = bst.ParamState(0.1)
+
+v_e = bst.ParamState(0.1 * u.meter ** 2 / u.second)
+K = bst.ParamState(0.001 * u.meter ** 2)
 
 
 def sol(x):
-    r = (v * e / (1e-3 * 1e-3)) ** 0.5
-    y = g * 1e-3 / v * (1 - np.cosh(r * (x['x'] - H / 2)) / np.cosh(r * H / 2))
-    return {'y': y}
+    x = x['x'].mantissa
+    r = (v.mantissa * e / (1e-3 * 1e-3)) ** 0.5
+    y = (
+        g.mantissa * 1e-3 /
+        v.mantissa *
+        (1 - u.math.cosh(r * (x - H.mantissa / 2)) /
+         u.math.cosh(r * H.mantissa / 2))
+    )
+    return {'y': y * u.meter / u.second}
 
-
-def gen_traindata(num):
-    xvals = np.linspace(1 / (num + 1), 1, num, endpoint=False)
-    x = {'x': xvals}
-    return x, sol(x)
+geom = pinnx.geometry.Interval(0, 1).to_dict_point(x=u.meter)
 
 
 def pde(x, y):
@@ -36,17 +37,21 @@ def pde(x, y):
     return -v_e.value / e * du_xx + v * y['y'] / K.value - g
 
 
-geom = pinnx.geometry.Interval(0, 1).to_dict_point('x')
+def gen_traindata(num):
+    xvals = np.linspace(1 / (num + 1), 1, num, endpoint=False)
+    x = {'x': xvals * u.meter}
+    return x, sol(x)
+
 ob_x, ob_u = gen_traindata(5)
 observe_u = pinnx.icbc.PointSetBC(ob_x, ob_u)
 
 net = pinnx.nn.Model(
-    pinnx.nn.DictToArray(x=None),
+    pinnx.nn.DictToArray(x=u.meter),
     pinnx.nn.FNN(
         [1] + [20] * 3 + [1], "tanh",
         output_transform=lambda x, y: x * (1 - x) * y
     ),
-    pinnx.nn.ArrayToDict(y=None),
+    pinnx.nn.ArrayToDict(y=u.meter / u.second),
 )
 
 problem = pinnx.problem.PDE(
@@ -61,52 +66,7 @@ problem = pinnx.problem.PDE(
     num_test=500,
 )
 
-variable = pinnx.callbacks.VariableValue([v_e, K], period=200, filename="../../examples/pinn_inverse/variables1.dat")
+variable = pinnx.callbacks.VariableValue([v_e, K], period=200, filename="./variables1.dat")
 trainer = pinnx.Trainer(problem, external_trainable_variables=[v_e, K])
 trainer.compile(bst.optim.Adam(0.001), metrics=["l2 relative error"]).train(iterations=30000, callbacks=[variable])
 trainer.saveplot(issave=True, isplot=True)
-
-lines = open("../../examples/pinn_inverse/variables1.dat", "r").readlines()
-vkinfer = np.array(
-    [
-        np.fromstring(
-            min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line), key=len),
-            sep=",",
-        )
-        for line in lines
-    ]
-)
-
-l, c = vkinfer.shape
-v_etrue = 1e-3
-ktrue = 1e-3
-
-plt.figure()
-plt.plot(
-    range(0, 200 * l, 200),
-    np.ones(vkinfer[:, 0].shape) * v_etrue,
-    color="black",
-    label="Exact",
-)
-plt.plot(range(0, 200 * l, 200), vkinfer[:, 0], "b--", label="Pred")
-plt.xlabel("Epoch")
-plt.yscale("log")
-plt.ylim(top=1e-1)
-plt.legend(frameon=False)
-plt.ylabel(r"$\nu_e$")
-
-plt.figure()
-plt.plot(
-    range(0, 200 * l, 200),
-    np.ones(vkinfer[:, 1].shape) * ktrue,
-    color="black",
-    label="Exact",
-)
-plt.plot(range(0, 200 * l, 200), vkinfer[:, 1], "b--", label="Pred")
-plt.xlabel("Epoch")
-plt.yscale("log")
-plt.ylim(ymax=1e-1)
-plt.legend(frameon=False)
-plt.ylabel(r"$K$")
-
-plt.show()
---
+++ docs/unit-examples-inverse.rst
@@ -0,0 +1,10 @@
+PINN Inverse Examples
+=====================
+
+.. toctree::
+    :maxdepth: 1
+
+    elliptic_inverse_filed.ipynb
+    brinkman_forchheimer.ipynb
+
+
